8d4c32a715187d821eb78514cd55cf1d844e0465
Emeka <ilozuechukwuemeka@yahoo.com>
2020-03-16 14:01:34 +0100

    Initial application generated by JHipster-6.0.0

--- Documents/PALLADIUM WORKS/LAMISPLUS 03022020/base/src/main/java/org/lamisplus/base/rest/FormController.java
+++ Documents/PALLADIUM WORKS/LAMISPLUS 03022020/base/src/main/java/org/lamisplus/base/rest/FormController.java
@@ -1,4 +1,30 @@
 package org.lamisplus.base.rest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.lamisplus.base.domain.entities.Form;
+
+import org.lamisplus.base.service.FormService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("/api/forms")
+@Slf4j
 public class FormController {
+    private final FormService formService;
+
+    public FormController(FormService formService) {
+
+        this.formService = formService;
+    }
+
+    @GetMapping("/all")
+    public List<Form> findAllForms() {
+        return this.formService.getAllForm();
+    }
+
+
 }
--- Documents/PALLADIUM WORKS/LAMISPLUS 03022020/base/src/main/java/org/lamisplus/base/service/FormService.java
+++ Documents/PALLADIUM WORKS/LAMISPLUS 03022020/base/src/main/java/org/lamisplus/base/service/FormService.java
@@ -1,4 +1,50 @@
 package org.lamisplus.base.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.lamisplus.base.domain.entities.Country;
+import org.lamisplus.base.domain.entities.Form;
+import org.lamisplus.base.repositories.CountriesRepository;
+import org.lamisplus.base.repositories.FormRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@Slf4j
 public class FormService {
+    private final FormRepository formRepository;
+
+    public FormService(FormRepository formRepository) {
+
+        this.formRepository = formRepository;
+    }
+
+    public List<Form> getAllForm() {
+        List<Form> forms = this.formRepository.findAll();
+
+        List<Form> formList = new ArrayList();
+        forms.forEach(oneForm -> {
+            Form form = new Form();
+
+            if(oneForm.getServiceName().equals("GENERAL_SERVICE"))
+                return;
+
+            form.setName(oneForm.getName());
+            form.setId(oneForm.getId());
+            form.setResourcePath(oneForm.getResourcePath());
+            form.setServiceName(oneForm.getServiceName());
+            form.setVersion(oneForm.getVersion());
+            form.setDisplayName(oneForm.getDisplayName());
+
+            formList.add(form);
+
+        });
+
+        return formList;
+    }
+
 }
